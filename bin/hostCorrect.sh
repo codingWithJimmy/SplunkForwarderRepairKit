ddce258761120252ce36dab5812cc34581856a53
Jimmy Maple <38733055+jimmyatSplunk@users.noreply.github.com>
2019-11-30 22:37:22 -0500

    Update hostCorrect.sh
    Implemented the actual command to change the files to the script so it wouldn't continually loop without making the correction.

--- bin/hostCorrect.sh
+++ bin/hostCorrect.sh
@@ -13,6 +13,7 @@ if [ $CURRENT_HOST = $HOSTNAME ]; then
 else
 	echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") ${HOSTNAME}: Currently configured host name: $CURRENT_HOST. Reconfiguring inputs.conf..."
 	cp $INPUTS_FILE $INPUTS_FILE.$(date +"%m%d%Y")
+        sed -i "s/$CURRENT_HOST/$HOSTNAME/" $INPUTS_FILE
 	touch $RESTART_INPUT_CHECK
 fi
 if [ $CURRENT_SERVER = $HOSTNAME ]; then
@@ -20,5 +21,6 @@ if [ $CURRENT_SERVER = $HOSTNAME ]; then
 else
 	echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") ${HOSTNAME}: Currently configured server name: $CURRENT_SERVER. Reconfiguring inputs.conf..."
 	cp $SERVER_FILE $SERVER_FILE.$(date +"%m%d%Y")
+        sed -i "s/$CURRENT_SERVER/$HOSTNAME/" $SERVER_FILE
 	touch $RESTART_SERVER_CHECK
 fi
