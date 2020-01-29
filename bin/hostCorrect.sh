da13af3150e1857c36876af3f7bb62eff585ab96
Jimmy Maple <jmaple@splunk.com>
2020-01-29 12:51:11 -0500

    Update hostCorrect.sh
    Update to determine if the local "inputs.conf" exists and to create it if it's missing.

--- bin/hostCorrect.sh
+++ bin/hostCorrect.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 ### Capture the existing host and serverName values and define path to restart checkpoint file
-INPUTS_FILE=$SPLUNK_HOME/etc/system/local/inputs.conf
-SERVER_FILE=$SPLUNK_HOME/etc/system/local/server.conf
-CURRENT_HOST=$(cat $INPUTS_FILE | grep "host =" | awk '{printf $3}')
-CURRENT_SERVER=$(cat $SERVER_FILE | grep "serverName =" | awk '{printf $3}')
+INPUTS_FILE="$SPLUNK_HOME/etc/system/local/inputs.conf"
+SERVER_FILE="$SPLUNK_HOME/etc/system/local/server.conf"
 RESTART_INPUT_CHECK="$SPLUNK_HOME/etc/restartinput.txt"
 RESTART_SERVER_CHECK="$SPLUNK_HOME/etc/restartserver.txt"
+CURRENT_SERVER=$(cat "$SERVER_FILE" | grep "serverName =" | awk '{printf $3}')
+
+### Check for the existence of the inputs file and create if it doesn't exist
+if [ -f "$INPUTS_FILE" ]; then
+	CURRENT_HOST=$(cat "$INPUTS_FILE" | grep "host =" | awk '{printf $3}')
+else
+	echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") ${HOSTNAME}: Inputs file is missing from local config. Creating it now..."
+	touch "$INPUTS_FILE"
+	echo "[default]
+	host = IntentionallyWrong" > "$INPUTS_FILE"
+	CURRENT_HOST=$(cat "$INPUTS_FILE" | grep "host =" | awk '{printf $3}')
+fi
 
 ### Compare those values and correct if necessary
-if [ $CURRENT_HOST = $HOSTNAME ]; then
+if [ "$CURRENT_HOST" = "$HOSTNAME" ]; then
 	echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") ${HOSTNAME}: Currently configured host name: $CURRENT_HOST. No correction necessary..."
 else
 	echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") ${HOSTNAME}: Currently configured host name: $CURRENT_HOST. Reconfiguring inputs.conf..."
-	cp $INPUTS_FILE $INPUTS_FILE.$(date +"%m%d%Y")
-	sed -i "s/$CURRENT_HOST/$HOSTNAME/" $INPUTS_FILE
-	touch $RESTART_INPUT_CHECK
+	cp "$INPUTS_FILE" "$INPUTS_FILE".$(date +"%m%d%Y")
+	sed -i "s/$CURRENT_HOST/$HOSTNAME/" "$INPUTS_FILE"
+	touch "$RESTART_INPUT_CHECK"
 fi
-if [ $CURRENT_SERVER = $HOSTNAME ]; then
+if [ "$CURRENT_SERVER" = "$HOSTNAME" ]; then
 	echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") ${HOSTNAME}: Currently configured server name: $CURRENT_SERVER. No correction necessary..."
 else
 	echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") ${HOSTNAME}: Currently configured server name: $CURRENT_SERVER. Reconfiguring inputs.conf..."
-	cp $SERVER_FILE $SERVER_FILE.$(date +"%m%d%Y")
-	sed -i "s/$CURRENT_SERVER/$HOSTNAME/" $SERVER_FILE
-	touch $RESTART_SERVER_CHECK
+	cp "$SERVER_FILE" "$SERVER_FILE".$(date +"%m%d%Y")
+	sed -i "s/$CURRENT_SERVER/$HOSTNAME/" "$SERVER_FILE"
+	touch "$RESTART_SERVER_CHECK"
 fi
