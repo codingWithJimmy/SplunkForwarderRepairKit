85928c222a59aa1c2e79076243ab08ab42acc79b
Jimmy Maple <jmaple@splunk.com>
2020-04-27 13:31:19 -0400

    Update to dsremove scripts
    Updates to the dsremove scripts so they perform the same actions with the same logic so they behave the same way

--- bin/dsRemove.ps1
+++ bin/dsRemove.ps1
@@ -1,19 +1,21 @@
 ### Configure file paths for the system
-$LOCAL = {$(Test-Path "$SPLUNKHOME\etc\system\local\deploymentclient.conf")}
-$DEPLOYED = {$(Test-Path "$SPLUNKHOME\etc\apps\*\*\deploymentclient.conf")}
+$LOCAL = {$((Get-ChildItem -Path "$SPLUNKHOME\etc\system\local" -Include deploymentclient.conf -File -Recurse).Count)}
+$DEPLOYED = {$((Get-ChildItem -Path "$SPLUNKHOME\etc\apps" -Include deploymentclient.conf -File -Recurse).Count)}
 $RESTART_CHECK = "$SPLUNKHOME\etc\restartds.txt"
 
 ### Filter to attach timestamps where necessary
 filter timestamp {"$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss.fff') ${env:COMPUTERNAME}: $_"}
 
-### Check to see if there is a deploymentclient.conf file under $SPLUNK_HOME\etc\apps and bail out if there isn't
-if ($DEPLOYED -eq "False") {
-    Write-output "No deploymentclient.conf detected in \$SPLUNK_HOME\etc\apps. Bailing out so the fowarder doesn't get orphaned." | timestamp
-} elseif ($LOCAL -eq "True") {
+### Check to see if there is a deploymentclient.conf file under $SPLUNKHOME\etc\apps and bail out if there isn't
+if ($DEPLOYED -eq "0") {
+    Write-output "No deploymentclient.conf detected in $SPLUNKHOME\etc\apps. Bailing out so the fowarder doesn't get orphaned." | timestamp
+} elseif ($DEPLOYED -gt "1") {
+    Write-output "Multiple deploymentclient.conf detected in $SPLUNKHOME\etc\apps. Check all deployed apps to ensure you\'re only using one." | timestamp
+} elseif ($LOCAL -eq "1" -AND $DEPLOYED -eq "1") {
     ### Remove the local "deploymentclient.conf" and flag
     Write-output "Removed deploymentclient.conf from local system." | timestamp
     Remove-Item -Path "$LOCAL"
     Out-File -FilePath "$RESTART_CHECK"
-} else { 
+} else {
     Write-output "No deploymentclient.conf correction necessary." | timestamp
 }
--- bin/dsRemove.sh
+++ bin/dsRemove.sh
@@ -5,14 +5,20 @@ LOCAL=$(find $SPLUNK_HOME/etc/system/local -type f -name deploymentclient.conf |
 RESTART_CHECK=$SPLUNK_HOME/etc/restartds.txt
 
 ### Check variables and take action accordingly
-if [ $LOCAL = "1" ] & [ $DEPLOYED_APP = "0" ]; then
-        echo $(date -R) $HOSTNAME: No deploymentclient.conf detected in \$SPLUNK_HOME/etc/apps. Bailing out so the fowarder doesn\'t get orphaned.
-        exit
-elif [ $LOCAL = "1" ] & [ $DEPLOYED_APP  = "1" ]; then
-		# Remove the deploymentclient.conf from $SPLUNK_HOME/etc/system/local
-		rm -f $SPLUNK_HOME/etc/system/local/deploymentclient.conf > /dev/null 2>&1
-		echo $(date -R) $HOSTNAME: Removed deploymentclient.conf from local system.
-		touch $RESTART_CHECK
-else
-		echo $(date -R) $HOSTNAME: No deploymentclient.conf correction necessary.
+## If there is no deployed app with a deployment client, bail out
+if [ $DEPLOYED_APP = "0" ]; then
+        echo $(date -R) $HOSTNAME: No deploymentclient.conf detected in $SPLUNK_HOME/etc/apps. Bailing out so the fowarder doesn\'t get orphaned.
+        exit 1
+      ## If more than one deploymentclient.conf file is deployed, bail out
+      elif [ $DEPLOYED_APP > "1" ]; then
+        echo $(date -R) $HOSTNAME: Multiple deploymentclient.conf detected in $SPLUNK_HOME/etc/apps. Check all deployed apps to ensure you\'re only using one.
+        exit 1
+      ## If there's 1 local config and 1 deployed config, remove the local one and set the checkpoint file
+      elif [ $LOCAL = "1" ] & [ $DEPLOYED_APP = "1" ]; then
+        # Remove the deploymentclient.conf from $SPLUNK_HOME/etc/system/local
+        rm -f $SPLUNK_HOME/etc/system/local/deploymentclient.conf > /dev/null 2>&1
+        echo $(date -R) $HOSTNAME: Removed deploymentclient.conf from local system.
+        touch $RESTART_CHECK
+      else
+        echo $(date -R) $HOSTNAME: No deploymentclient.conf correction necessary.
 fi
