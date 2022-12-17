4e56c8152e2c2b2f18bb9220f27339d162178bfb
Jimmy Maple <jmaple@splunk.com>
2022-12-17 10:12:21 -0500

    Update btoolOutput.sh
    - Updated printed output with correct variables

--- bin/btoolOutput.sh
+++ bin/btoolOutput.sh
@@ -8,11 +8,11 @@ BTOOL_OUTPUT=$(${SPLUNK_HOME}/bin/splunk btool $1 list --debug)
 
 ### Print btool output taking into account whether "system\default" is included
 if [ "${INCLUDE_DEFAULT}" = "false" ] || [ "${INCLUDE_DEFAULT}" = "0" ]; then
-  echo "$(date -R) ${HOSTNAME}: btool output for ${BTOOL_FILE}.conf"
+  echo "$(date -R) ${HOSTNAME}: btool output for ${1}.conf"
   echo "============================================================================"
-  echo "${BTOOL_OUTPUT} | grep -v 'system/default"
+  echo "${BTOOL_OUTPUT} | grep -v 'system/default'"
 else
-  echo "$(date -R) ${HOSTNAME}: btool output for ${BTOOL_FILE}.conf"
+  echo "$(date -R) ${HOSTNAME}: btool output for ${1}.conf"
   echo "============================================================================"
   echo "${BTOOL_OUTPUT}"
 fi
