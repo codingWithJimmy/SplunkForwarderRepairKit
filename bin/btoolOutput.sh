373fd618744520a678376e7c5e9f3bcd4fe9cf99
Jimmy Maple <jmaple@splunk.com>
2022-12-17 10:10:55 -0500

    Update btoolOutput.sh
    - Updated validation check logic for including default output

--- bin/btoolOutput.sh
+++ bin/btoolOutput.sh
@@ -7,7 +7,7 @@
 BTOOL_OUTPUT=$(${SPLUNK_HOME}/bin/splunk btool $1 list --debug)
 
 ### Print btool output taking into account whether "system\default" is included
-if [ "${INCLUDE_DEFAULT}" -eq "false" ] || [ "${INCLUDE_DEFAULT}" -eq "0" ]; then
+if [ "${INCLUDE_DEFAULT}" = "false" ] || [ "${INCLUDE_DEFAULT}" = "0" ]; then
   echo "$(date -R) ${HOSTNAME}: btool output for ${BTOOL_FILE}.conf"
   echo "============================================================================"
   echo "${BTOOL_OUTPUT} | grep -v 'system/default"
