a8f82b009fb6890363b4e1f42b4426ffd7e899a9
Jimmy Maple <38733055+jimmyatSplunk@users.noreply.github.com>
2022-12-25 23:54:26 -0500

    Update appContext.sh
    - Fixed issues with appContext.sh variable identification

--- bin/appContext.sh
+++ bin/appContext.sh
@@ -3,7 +3,7 @@
 SCRIPT_PATH=$(realpath $0)
 APP_PATH=$(dirname ${SCRIPT_PATH})
 SCRIPT_NAME=$(echo ${SCRIPT_PATH} | sed "s|${APP_PATH}/||")
-APP_NAME=$(echo ${SCRIPT_PATH} | sed "s|${SPLUNK_HOME}/etc/apps||" | sed "s|/${SCRIPT_NAME}||")
+APP_NAME=$(echo ${SCRIPT_PATH} | sed "s|${SPLUNK_HOME}/etc/apps/||" | sed "s|/bin/${SCRIPT_NAME}||")
 
 ## Capture the deploymentServerUri from the inputs stanza
 if [ "${SCRIPT_NAME}" = "dsRemove.sh" ]; then
@@ -21,6 +21,6 @@ fi
 
 ## Capture the configuration details for btool outputs
 if [ "${SCRIPT_NAME}" = "btoolOutput.sh" ]; then
-  BTOOL_INPUT=$(${SPLUNK_HOME}/bin/splunk btool --app=${APP_NAME} inputs list "script://./bin/btoolOutput.sh $1" --debug | grep include_default)
-  INCLUDE_DEFAULT=$(echo ${BTOOL_INPUT} | awk '{print $4}')
+  BTOOL_INPUT=$(${SPLUNK_HOME}/bin/splunk btool --app=${APP_NAME} inputs list "script://./bin/btoolOutput.sh $1" | grep include_default)
+  INCLUDE_DEFAULT=$(echo ${BTOOL_INPUT} | awk '{print $3}')
 fi
